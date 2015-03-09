6d47e00f83867373c05c42767baf91f1f237b23c
anjana-babu <anjana.babu@gmail.com>
2015-03-09 21:44:30 +0530

    kmeans

---
+++ kmeans/RandomEuclidean.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Using Random Means - Euclidean Distance */
+int main(int argc, char *argv[])
+{
+
+
+  double x[1000];
+  char class[1000];
+  double mean = 0.0, mean1 = 0.0, mean2 = 0.0, mean1_prev = 0.0, mean2_prev = 0.0;
+  double variance = 0.0, variance1 = 0.0, variance2 = 0.0, variance1_prev = 0.0, variance2_prev = 0.0;
+  int i=0;
+  FILE *fp;
+  char s[20];
+  int np, count_class1, count_class2,r;
+
+  /*
+   * Training data
+   */
+  fp = fopen("trainData.txt","rb");
+  if (fp == NULL)
+  {
+     perror("Failed to open file \"myfile\"");
+     return EXIT_FAILURE;
+  }
+
+  /* Reading all the values from a file */
+  while(!feof(fp))
+  {
+     fscanf(fp,"%lf",&x[i]);
+     i++;
+  }
+  np = i;
+  fclose(fp);
+
+  /* Calculating mean for all the points */
+  srand ( time(NULL) );
+
+  /* generate random means */
+  r = (rand() % 800 );
+  mean1 = x[r];
+  r = (rand() % 800 );
+  mean2 = x[r];
+
+  printf("\n Random mean 1=%lf Random mean 2= %lf",mean1,mean2);
+
+  do
+  {
+     for(i = 0;i < np;i++)
+     {
+        if((sqrt((x[i]-mean1)*(x[i]-mean1)))<(sqrt((x[i]-mean2)*(x[i]-mean2))))
+        {
+           class[i] = 1;
+        }
+        else
+        {
+           class[i] = 2;
+        }
+     }
+
+     /* Computing mean for 2 classes */
+     mean1_prev = mean1;
+     mean2_prev = mean2;
+     count_class1 = 0;
+     count_class2 = 0;
+     mean1 = 0;
+     mean2 = 0;
+
+     for(i = 0;i < np;i++)
+     {
+        if(class[i] == 1)
+        {
+           mean1 += x[i];
+           count_class1++;
+        }
+        else
+        {
+           mean2 += x[i];
+           count_class2++;
+        }
+     }
+     mean1 /= count_class1;
+     mean2 /= count_class2;
+
+     /* Computing variance for 2 classes */
+     variance1_prev = variance1;
+     variance2_prev = variance2;
+     count_class1 = 0;
+     count_class2 = 0;
+     variance1 = 0;
+     variance2 = 0;
+
+     for(i = 0;i < np;i++)
+     {
+        if(class[i] == 1)
+        {
+           variance1 += (x[i]-mean1) * (x[i]-mean1);
+           count_class1++;
+        }
+        else
+        {
+           variance2 += (x[i]-mean2) * (x[i]-mean2);
+           count_class2++;
+        }
+     }
+     variance1 /= count_class1;
+     variance2 /= count_class2;
+
+  }while(mean1!=mean1_prev && mean2!=mean2_prev);
+
+  printf("\n Mean of class 1 = %lf",mean1);
+  printf("\n Variance of class 1 = %lf", variance1);
+  printf("\n Mean of class 2 = %lf",mean2);
+  printf("\n Variance of class 2 = %lf", variance2);
+
+  /*
+   * Testing data
+   */
+
+  fp = fopen("testData.txt","rb");
+  if (fp == NULL)
+  {
+     perror("Failed to open file \"myfile\"");
+     return EXIT_FAILURE;
+  }
+
+  /* Reading all the values from a file */
+  i = 0;
+  while(!feof(fp))
+  {
+     fscanf(fp,"%lf",&x[i]);
+     i++;
+  }
+  np = i;
+  fclose(fp);
+printf("\n number of test data:  %d",np);
+  count_class1 = 0;
+  count_class2 = 0;
+  for(i = 0;i < np ;i++)
+  {
+     if((sqrt((x[i]-mean1)*(x[i]-mean1)/variance1))<(sqrt((x[i]-mean2)*(x[i]-mean2)/variance2)))
+     {
+        class[i] = 1;
+        count_class1++;
+     }
+     else
+     {
+        class[i] = 2;
+        count_class2++;
+     }
+  }
+  printf("\n Testing first 100 points.. ");
+  printf("\n Class 1 : %d, Class 2 : %d", count_class1, count_class2);
+
+  /* count_class1 = 0;
+  count_class2 = 0;
+  for(i = (np - 100);i < np ;i++)
+  {
+     if((sqrt((x[i]-mean1)*(x[i]-mean1)))<(sqrt((x[i]-mean2)*(x[i]-mean2))))
+     {
+        class[i] = 1;
+        count_class1++;
+     }
+     else
+     {
+        class[i] = 2;
+        count_class2++;
+     }
+  }
+  printf("\n Testing next 100 points..");
+  printf("\n Class 1 : %d, Class 2 : %d", count_class1, count_class2);*/
+
+  system("PAUSE");
+  return 0;
+}
